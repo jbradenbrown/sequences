d924a6bf50c2bf32938545010a382317ccc611d1
Jeffrey Brown <jeffrey@sabrient.ai>
2019-02-25 00:04:03 -0600

    better?

--- src/Pattern.hs
+++ src/Pattern.hs
@@ -7,10 +7,13 @@ import Data.Text (Text)
 type Pattern a = [a]
 
 candidates :: [[Text]] -> [Pattern Text] -> [Pattern Text]
-candidates t xs = concat $ map (\l -> candidates' l $ concat $ filter (`isInfixOf` l) t) xs
+candidates t xs = concat $ map (\l -> candidates' l $ concat $ filter (isInfixOf l) t) xs
 
 candidates' :: Pattern Text -> [Text] -> [Pattern Text]
-candidates' p all = [p ++ [a] | a <- all]
+candidates' _ [] = []
+candidates' p all@(x:xs) = case p `isPrefixOf` all of
+  True -> [take (genericLength p + 1) all] ++ candidates' p xs
+  False -> candidates' p xs
 
 instanceOf :: (Eq a, Ord a) => Pattern a -> [a] -> Bool
 instanceOf _ [] = False
