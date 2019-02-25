ff07b3c19f87f795ad15546fb6a8ac752b72f082
Jeffrey Brown <jeffrey@sabrient.ai>
2019-02-25 01:00:43 -0600

    iwannadie

--- src/Apriori.hs
+++ src/Apriori.hs
@@ -11,8 +11,8 @@ apriori s t = (apriori' s t) . frequent' $ singletons t
 
 apriori' :: (Num b, Ord b) => b -> [[Text]] -> [(Pattern Text, b)] -> [(Pattern Text, b)]
 apriori' _ _ [] = []
-apriori' s t ps = ps ++ (apriori' s t $ frequent' $ candidates t $ map fst $ ps)
-  where frequent' xs = (frequent (genericLength t) s) $ foldl' (\acc n -> acc ++ filter (`isInfixOf` n) xs) [] t
+apriori' s t ps = (apriori' s t $ frequent' $ candidates t $ map fst $ ps) ++ ps
+  where frequent' xs = (frequent (genericLength t) s) $ foldl' (\acc n -> acc ++ filter (isInfixOf n) t) [] xs
 
 count :: (Eq a, Ord a, Num b) => [Pattern a] -> [(Pattern a, b)]
 count = map (\x -> (head x, genericLength x)) . group . sort
